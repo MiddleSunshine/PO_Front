ebfce73de1d528b4274f513e91d0f7d880107921
Adam <qingxianyang@gmail.com>
2021-10-09 19:24:40 +0800

    点击添加事件完成

--- src/component/Schedule.js
+++ src/component/Schedule.js
@@ -1,55 +1,132 @@
 import React from 'react'
-import { Calendar, Badge,Modal, Button,Switch,Row,Col } from 'antd';
+import {Calendar, Badge, Modal, Button, Switch, Row, Col, Form, Select, Input, DatePicker, message} from 'antd';
 import Road from './road';
 import {PlusCircleOutlined} from '@ant-design/icons';
-var dateFormat="YYYY-MM-DD";
+import SimpleMDE from "react-simplemde-editor";
+import moment from "moment";
+import {requestApi} from "../config/functions";
 
-class Schedule extends React.Component{
-    constructor(props){
+var dateFormat = "YYYY-MM-DD HH:mm:ss";
+
+class Schedule extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            plans:[],
-            plan:{
-                selectedDate:"2021-09-30",
-                planDetail:[]
+        this.state = {
+            plans: [],
+            plan: {
+                selectedDate: "2021-09-30",
+                planDetail: []
             },
-            modalVisible:false,
-            newModalVisible:true,
+            planItem:{},
+            activePlans:[],
+            modalVisible: false,
+            newModalVisible: false,
         };
-        this.dayRender=this.dayRender.bind(this);
-        this.closeModal=this.closeModal.bind(this);
-        this.openModal=this.openModal.bind(this);
+        this.dayRender = this.dayRender.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+        this.openModal = this.openModal.bind(this);
+        this.openNewPlanItemModal=this.openNewPlanItemModal.bind(this);
+        this.handleModalInputChange=this.handleModalInputChange.bind(this);
+        this.savePlanItem=this.savePlanItem.bind(this);
+        this.getActivePlans=this.getActivePlans.bind(this);
     }
-    dayRender(dateMoment){
-        let date=dateMoment.format(dateFormat);
+
+    componentDidMount() {
+        this.getActivePlans();
+    }
+
+    dayRender(dateMoment) {
+        let date = dateMoment.format(dateFormat);
         return <div>
-            <Button
-                icon={<PlusCircleOutlined />}
-            ></Button>
-            <Button
-                    onClick={()=>this.openModal(date)}
-                >
-                    <span>{date}</span>
-                </Button>
+            <Row>
+                <Col span={24}>
+                    <Button
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            // todo 这里编辑历史任务
+                        }}
+                    >
+
+                    </Button>
+                </Col>
+            </Row>
         </div>
     }
-    closeModal(){
+
+    closeModal() {
         this.setState({
-            modalVisible:false
+            modalVisible: false,
+            newModalVisible:false
         });
     }
-    openModal(date){          
+
+    openModal(date) {
         this.setState({
-            plan:{
-                selectedDate:date                     
+            plan: {
+                selectedDate: date
             },
-            modalVisible:true
+            modalVisible: true
         });
     }
-    render(){
+
+    openNewPlanItemModal(date){
+        this.setState({
+            planItem:{
+                FinishTime:date.format(dateFormat).toString()
+            },
+            newModalVisible:true
+        })
+    }
+
+    handleModalInputChange(key,value){
+        let planItem=this.state.planItem;
+        planItem[key]=value;
+        this.setState({
+            planItem:planItem
+        });
+    }
+
+    savePlanItem(){
+        let planItem=this.state.planItem;
+        if (!planItem.FinishTime){
+            planItem.FinishTime=moment().format(dateFormat).toString();
+        }
+        requestApi("/index.php?action=PlanItem&method=SaveWithoutPPID",{
+            method:"post",
+            mode:"cors",
+            body:JSON.stringify(planItem)
+        })
+            .then((res)=>{
+                res.json().then((json)=>{
+                    if (json.Status==1){
+                        this.closeModal();
+                        return true;
+                    }else{
+                        return  false;
+                    }
+                })
+                    .then((result)=>{
+                        if (result){
+                            message.success("Save Success !")
+                        }
+                    })
+            })
+    }
+
+    getActivePlans(){
+        requestApi("/index.php?action=Plan&method=GetActivePlan")
+            .then((res)=>{
+                res.json().then((json)=>{
+                    this.setState({
+                        activePlans:json.Data
+                    })
+                })
+            })
+    }
+
+    render() {
         return <div>
-            <hr/>
-            <Road />
+            <Road/>
             <hr/>
             <Row>
                 <Col span={2}>
@@ -58,7 +135,7 @@ class Schedule extends React.Component{
                 <Col span={2}>
                     <Switch
                         checkedChildren="Show"
-                        unCheckedChildren="Hide"                                     
+                        unCheckedChildren="Hide"
                     />
                 </Col>
                 <Col span={2}>
@@ -67,28 +144,96 @@ class Schedule extends React.Component{
                 <Col span={2}>
                     <Switch
                         checkedChildren="Show"
-                        unCheckedChildren="Hide"                                     
+                        unCheckedChildren="Hide"
                     />
-                </Col>               
+                </Col>
             </Row>
             <hr/>
             <Calendar
-            dateCellRender={this.dayRender}        
-         />
-         <Modal
-            visible={this.state.modalVisible}
-            title={"Date : "+this.state.plan.selectedDate}
-            onOk={()=>this.closeModal()}
-            onCancel={()=>this.closeModal()}
-         >
-            这里展示详细数据
-         </Modal>
-         <Modal
-            visible={this.state.newModalVisible}
-            title={"New Plan"}
-         >
-
-         </Modal>
+                dateCellRender={this.dayRender}
+                onSelect={(date)=>{
+                    this.openNewPlanItemModal(date);
+                }}
+            />
+            <Modal
+                visible={this.state.modalVisible}
+                title={"Date : " + this.state.plan.selectedDate}
+                onOk={() => this.closeModal()}
+                onCancel={() => this.closeModal()}
+                width={1800}
+            >
+                这里展示详细数据
+            </Modal>
+            <Modal
+                visible={this.state.newModalVisible}
+                title={"New Plan"}
+                width={1800}
+                onCancel={()=>{
+                    this.closeModal();
+                }}
+                onOk={()=>{
+                    this.savePlanItem();
+                }}
+            >
+                <Form
+                    layout="vertical"
+                >
+                    <Form.Item
+                        label={"Plans"}
+                    >
+                        <Select
+                            value={this.state.planItem.PID}
+                            onChange={(value)=>{
+                                this.handleModalInputChange('PID',value)
+                            }}
+                        >
+                            {
+                                this.state.activePlans.map((Item)=>{
+                                    return(
+                                        <Select.Option value={Item.ID}>
+                                            {Item.Name}
+                                        </Select.Option>
+                                    )
+                                })
+                            }
+                        </Select>
+                    </Form.Item>
+                    <Form.Item
+                        label={"Name"}
+                    >
+                        <Input
+                            value={this.state.planItem.Name}
+                            onChange={(e)=>{
+                                this.handleModalInputChange('Name',e.target.value);
+                            }}
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        label={"Note"}
+                    >
+                        <SimpleMDE
+                            value={this.state.planItem.Note}
+                            onChange={(value)=>{
+                                this.handleModalInputChange('Note',value);
+                            }}
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        label={"Finish Time"}
+                    >
+                        <DatePicker
+                            showTime={true}
+                            defaultValue={this.state.planItem.FinishTime?moment(this.state.planItem.FinishTime,dateFormat):moment()}
+                            onChange={(date,dateString)=>{
+                                this.handleModalInputChange(
+                                    'FinishTime',
+                                    dateString
+                                )
+                            }}
+                        />
+                    </Form.Item>
+                </Form>
+            </Modal>
         </div>;
     }
 }
