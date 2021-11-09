4c67881967dde549fd0b3915355cf6ff46e845e8
yangqingxian <qingxianyang@gmail.com>
2021-11-09 01:50:55 -0800

    暂存部分页面效果

--- src/page/PointsRoad.js
+++ src/page/PointsRoad.js
@@ -6,6 +6,7 @@ import config from "../config/setting";
 import "../css/PointRoad.css"
 import {Button, Col, Drawer, InputNumber, Row, Tooltip} from "antd";
 import PointEdit from "../component/PointEdit";
+import {message} from "antd/es";
 
 class PointsRoad extends React.Component{
     constructor(props) {
@@ -16,14 +17,40 @@ class PointsRoad extends React.Component{
             pid:props.match.params.pid,
             EditPoint:{},
             pointItemWidth:130,
-            pointItemHeight:80,
+            pointItemHeight:80
         }
         this.getTableData=this.getTableData.bind(this);
         this.EditPoint=this.EditPoint.bind(this);
+        this.updateConnection=this.updateConnection.bind(this);
     }
 
     componentDidMount() {
         this.getTableData(this.state.pid);
+    }
+
+    updateConnection(PID,SubPID){
+        console.log("PID",PID);
+        console.log("SubPID",SubPID);
+        if (PID && PID>0 && SubPID && SubPID>0){
+            requestApi("/index.php?action=PointsConnection&method=NewConnection&StartID="+this.state.pid+"&PID="+PID+"&SubPID="+SubPID)
+                .then((res)=>{
+                    res.json().then((json)=>{
+                        if (json.Status==1){
+                            message.success("New Connection !");
+                            return true;
+                        }else{
+                            message.warn(json.Message);
+                            return false;
+                        }
+                    })
+                        .then((getDataAgain)=>{
+                            if (getDataAgain){
+                                this.getTableData(this.state.pid);
+                            }
+                        })
+                })
+        }
+
     }
 
     getTableData(id){
@@ -157,7 +184,20 @@ class PointsRoad extends React.Component{
                                                             break;
                                                     }
                                                     return(
-                                                        <td key={insideIndex}>
+                                                        <td
+                                                            key={insideIndex}
+                                                            draggable={"true"}
+                                                            onDragStart={(event)=>{
+                                                                event.dataTransfer.setData("SubPID",Item.Data.ID);
+                                                            }}
+                                                            onDrop={(event)=>{
+                                                                let PID=event.dataTransfer.getData("SubPID");
+                                                                this.updateConnection(PID,Item.Data.ID);
+                                                            }}
+                                                            onDragOver={(event)=>{
+                                                                event.preventDefault();
+                                                            }}
+                                                        >
                                                             {component}
                                                         </td>
                                                     )
