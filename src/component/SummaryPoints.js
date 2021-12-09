b9188690af7ca47560a004585639b4eeaaca4b23
yangqingxian <qingxianyang@gmail.com>
2021-12-08 21:05:56 -0800

    point与summary的效果完成

--- src/component/SummaryPoints.js
+++ src/component/SummaryPoints.js
@@ -1,4 +1,4 @@
-import { Button, Checkbox, Col, Drawer, Input, Row } from "antd";
+import { Button, Checkbox, Col, Drawer, Input, message, Row } from "antd";
 import React from "react";
 import { requestApi } from "../config/functions";
 
@@ -7,9 +7,15 @@ class SummaryPoints extends React.Component {
         super(props);
         this.state = {
             ID: props.ID,
+            // search part
             pointList: [],
+            //
+            searchPointList: [],
             searchPoint: "",
-            isSearchPoint: true
+            isSearchPoint: false,
+            // point list part
+            selectedPointList: [],
+            selectedPointIds: {}
         }
         this.getPoints = this.getPoints.bind(this);
         this.searchPoint = this.searchPoint.bind(this);
@@ -18,29 +24,109 @@ class SummaryPoints extends React.Component {
         this.switchPointSelector = this.switchPointSelector.bind(this);
     }
 
+    componentDidMount() {
+        this.getPoints(this.state.ID);
+    }
+
     getPoints(SummaryID) {
         if (SummaryID) {
             requestApi("/index.php?action=PointSummaryConnection&method=GetPoints&ID=" + SummaryID)
                 .then((res) => {
                     res.json().then((json) => {
+                        let pointIds = {};
+                        json.Data.Points.map((point) => {
+                            pointIds[parseInt(point.ID)] = point;
+                        })
                         this.setState({
-                            pointList: json.Data.Points
+                            pointList: json.Data.Points,
+                            selectedPointIds: pointIds
                         })
                     })
                 })
         }
     }
 
-    searchPoint() {
-
+    searchPoint(keyword) {
+        if (!keyword) {
+            message.warn("Please input the keyword");
+            return false;
+        }
+        requestApi("/index.php?action=Points&method=Search", {
+            mode: "cors",
+            method: "post",
+            body: JSON.stringify({
+                keyword: keyword
+            })
+        }).then((res) => {
+            res.json().then((json) => {
+                this.setState({
+                    searchPointList: json.Data
+                });
+                return json.Data.length;
+            })
+                .then((searchAmount) => {
+                    message.success("Search Amount Is " + searchAmount);
+                })
+        })
     }
 
-    newPointConnection() {
-
+    newPointConnection(PID, updateList = true) {
+        if (this.state.ID) {
+            requestApi("/index.php?action=PointSummaryConnection&method=NewConnection", {
+                mode: "cors",
+                method: "post",
+                body: JSON.stringify({
+                    PID: PID,
+                    SID: this.state.ID
+                })
+            })
+                .then((res) => {
+                    res.json().then((json) => {
+                        if (json.Status == 1) {
+                            updateList && this.getPoints(this.state.ID);
+                            return true;
+                        }
+                        return false;
+                    })
+                        .then((isSuccess) => {
+                            if (isSuccess) {
+                                message.success("Connect Success");
+                            } else {
+                                message.warn("Connection Fail")
+                            }
+                        })
+                })
+        }
     }
 
-    deletePointConnection() {
-
+    deletePointConnection(PID, updateList = true) {
+        if (this.state.ID) {
+            requestApi("/index.php?action=PointSummaryConnection&method=Delete", {
+                mode: "cors",
+                method: "post",
+                body: JSON.stringify({
+                    PID: PID,
+                    SID: this.state.ID
+                })
+            })
+                .then((res) => {
+                    res.json().then((json) => {
+                        if (json.Status == 1) {
+                            updateList && this.getPoints(this.state.ID);
+                            return true;
+                        }
+                        return false;
+                    })
+                        .then((isSuccess) => {
+                            if (isSuccess) {
+                                message.success("Delete Connection");
+                            }
+                            else {
+                                message.error("Delete Connection Fail");
+                            }
+                        })
+                })
+        }
     }
 
     switchPointSelector(open = true) {
@@ -62,6 +148,38 @@ class SummaryPoints extends React.Component {
                     New Point
                 </Button>
             </Row>
+            {
+                this.state.pointList.map((point, index) => {
+                    return (
+                        <Row
+                            key={index}
+                            justify={"start"}
+                            align={"middle"}
+                            style={{ paddingTop: "5px" }}
+                        >
+                            <Col span={1}>
+                                <Checkbox
+                                    onChange={(e) => {
+                                        if (e.target.checked) {
+                                            this.newPointConnection(point.ID);
+                                        }
+                                    }}
+                                    checked={this.state.selectedPointIds[parseInt(point.ID)] ? true : false}
+                                />
+                            </Col>
+                            <Col span={22} offset={1}>
+                                <Button
+                                    type={"link"}
+                                    target={"_blank"}
+                                    href={"/point/edit/" + point.ID}
+                                >
+                                    {point.keyword}
+                                </Button>
+                            </Col>
+                        </Row>
+                    )
+                })
+            }
             <Row>
                 <Drawer
                     placement={"left"}
@@ -83,25 +201,36 @@ class SummaryPoints extends React.Component {
                                     })
                                 }}
                                 onPressEnter={() => {
-                                    this.searchPoint();
+                                    this.searchPoint(this.state.searchPoint);
                                 }}
                             />
                         </Col>
                     </Row>
                     {
-                        this.state.pointList.map((point, index) => {
+                        this.state.searchPointList.map((point, index) => {
                             return (
                                 <Row
                                     key={index}
+                                    justify={"start"}
+                                    align={"middle"}
                                 >
                                     <Col span={2}>
-                                        <Checkbox />
+                                        <Checkbox
+                                            checked={this.state.selectedPointIds[parseInt(point.ID)] ? true : false}
+                                            onChange={(e) => {
+                                                if (e.target.checked) {
+                                                    this.newPointConnection(point.ID);
+                                                } else {
+                                                    this.deletePointConnection(point.ID);
+                                                }
+                                            }}
+                                        />
                                     </Col>
                                     <Col span={22}>
                                         <Button
                                             type={"link"}
                                             target={"_blank"}
-                                            href={""}
+                                            href={"/point/edit/" + point.ID}
                                         >
                                             {
                                                 point.keyword
