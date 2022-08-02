8e92557469dd7bbb139f441e431dd79691537902
yangqingxian <qingxianyang@gmail.com>
2022-08-02 02:15:16 -0700

    基础代码框架完成

--- src/page/WhiteBoard.js
+++ src/page/WhiteBoard.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import {Tldraw} from "@tldraw/tldraw";
+import {requestApi} from "../config/functions";
+import {message} from "antd/es";
+import {Button} from "antd";
 
 class WhiteBoard extends React.Component{
     constructor(props) {
@@ -8,11 +11,74 @@ class WhiteBoard extends React.Component{
             document:{},
             ProjectName:props.match.params.ProjectName
         }
+        this.getDocument=this.getDocument.bind(this);
+        this.saveProject=this.saveProject.bind(this);
+    }
+
+    componentDidMount() {
+        this.getDocument(this.state.ProjectName);
+    }
+
+    getDocument(filePath){
+        requestApi("/index.php?action=WhiteBoard&method=ProjectDetail",{
+            method:"post",
+            mode:"cors",
+            body:JSON.stringify({
+                ProjectName:filePath
+            })
+        })
+            .then((res)=>{
+                res.json().then((json)=>{
+                    if (json.Status==1){
+                        this.setState({
+                            document:JSON.parse(json.Data.document)
+                        });
+                    }else{
+                        message.warn(json.Message)
+                    }
+                })
+            })
+    }
+
+    saveProject(){
+        requestApi("/index.php?action=WhiteBoard&method=SaveProject",{
+            mode:"cors",
+            method:"post",
+            body:JSON.stringify({
+                ProjectName:this.state.ProjectName,
+                document:this.state.document
+            })
+        })
+            .then((res)=>{
+                res.json().then((json)=>{
+                    if (json.Status==1){
+                        message.success("Save Success");
+                    }else{
+                        message.warn(json.Message);
+                    }
+                })
+            })
     }
 
     render() {
         return <div>
-            <Tldraw />
+            <Button
+                type={"primary"}
+                onClick={()=>{
+                    this.saveProject();
+                }}
+            >
+                Save Project
+            </Button>
+            <hr/>
+            <Tldraw
+                document={this.state.document}
+                onChange={(app)=>{
+                    this.setState({
+                        document:app.document
+                    })
+                }}
+            />
         </div>
     }
 }
