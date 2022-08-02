fc3f03b9579d9581811f6b4f0cda98668a81a58b
yangqingxian <qingxianyang@gmail.com>
2022-08-02 01:52:56 -0700

    whiteboard 组件完成

--- src/component/WhiteBoardList.js
+++ src/component/WhiteBoardList.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {requestApi} from "../config/functions";
-import {message} from "antd/es";
-import {Button, List} from "antd";
+import {Button, List, message, Row, Col, Input} from "antd";
 import {
     FormOutlined,
     DeleteOutlined
@@ -17,6 +16,7 @@ class WhiteBoardList extends React.Component{
         }
         this.getProjects=this.getProjects.bind(this);
         this.deleteProject=this.deleteProject.bind(this);
+        this.newProject=this.newProject.bind(this);
     }
 
     componentWillReceiveProps(nextProps, nextContext) {
@@ -65,36 +65,80 @@ class WhiteBoardList extends React.Component{
             })
     }
 
+    newProject(newProjectName){
+        if (!newProjectName){
+            message.warn("Please input the project name");
+            return false;
+        }
+        requestApi("/index.php?action=WhiteBoard&method=NewProject",{
+            mode:"cors",
+            method:"post",
+            body:JSON.stringify({
+                ProjectName:newProjectName,
+                PID:this.state.PID
+            })
+        })
+            .then((res)=>{
+                res.json().then((json)=>{
+                    if (json.Status==1){
+                        message.success("Create New Project");
+                        this.getProjects(this.state.PID);
+                    }else{
+                        message.warn(json.Message);
+                    }
+                })
+            })
+    }
+
 
 
     render() {
         return <div>
-            <List
-                dataSource={this.state.Projects}
-                renderItem={(project)=>{
-                    return(
-                        <List.Item
-                            key={project.FileName}
-                            actions={[
-                                <Button
-                                    type={"link"}
-                                    href={"/WhiteBoard/"+project.FilePath}
-                                    target={"_blank"}
-                                    icon={<FormOutlined />}
-                                ></Button>,
-                                <Button
-                                    type={"link"}
-                                    danger={true}
-                                    icon={<DeleteOutlined />}
-                                    onClick={()=>{
-                                        this.deleteProject(project.FilePath)
-                                    }}
-                                ></Button>
-                            ]}
-                        />
-                    )
-                }}
-            />
+            <Row>
+                <Col span={24}>
+                    <Input
+                        value={this.state.newProjectName}
+                        onChange={(e)=>{
+                            this.setState({
+                                newProjectName:e.target.value
+                            })
+                        }}
+                        onPressEnter={()=>{
+                            this.newProject(this.state.newProjectName)
+                        }}
+                    />
+                </Col>
+            </Row>
+            <Row>
+                <Col span={24}>
+                    <List
+                        dataSource={this.state.Projects}
+                        renderItem={(project)=>{
+                            return(
+                                <List.Item
+                                    key={project.FileName}
+                                    actions={[
+                                        <Button
+                                            type={"link"}
+                                            href={"/WhiteBoard/"+project.FilePath}
+                                            target={"_blank"}
+                                            icon={<FormOutlined />}
+                                        ></Button>,
+                                        <Button
+                                            type={"link"}
+                                            danger={true}
+                                            icon={<DeleteOutlined />}
+                                            onClick={()=>{
+                                                this.deleteProject(project.FilePath)
+                                            }}
+                                        ></Button>
+                                    ]}
+                                />
+                            )
+                        }}
+                    />
+                </Col>
+            </Row>
         </div>
     }
 }
