f6ec014ced5824d895f14745ab0a11472c2b123c
qingxianyang <qingxianyang@gmail.com>
2022-11-08 22:43:53 +0800

    完成部分页面效果

--- src/mobile/Mind.js
+++ src/mobile/Mind.js
@@ -1,61 +1,157 @@
 import React from 'react'
-import {AutoCenter, Avatar, Button, CapsuleTabs, Divider, Grid, Space, TextArea} from "antd-mobile";
-import {SmileOutline,FrownOutline,StarOutline,HeartOutline} from 'antd-mobile-icons';
+import {
+    AutoCenter,
+    Avatar,
+    Button,
+    CapsuleTabs,
+    Divider, DotLoading,
+    Grid,
+    Image,
+    ImageViewer,
+    Mask, Rate,
+    Space,
+    TextArea
+} from "antd-mobile";
+import {SmileOutline,FrownOutline,StarOutline,HeartOutline,CameraOutline} from 'antd-mobile-icons';
 
 import "../css/Mind.css";
+import {ImageUploader} from "antd-mobile/2x";
+import {BUCKET_LONG_FILE, BUCKET_TEMP_FILE, uploadFile} from "../component/imageUpload";
 
 class Mind extends React.Component {
     constructor(props) {
         super(props);
         this.state={
-
+            activeKey:-1,
+            type:"",
+            note:"",
+            ImageUrl:[],
+            rate:0
         }
+        this.HandleUpload=this.HandleUpload.bind(this);
+        this.updateType=this.updateType.bind(this);
+        this.saveMind=this.saveMind.bind(this);
+    }
+
+    async HandleUpload(fileObject){
+        let url=await uploadFile(BUCKET_TEMP_FILE,fileObject);
+        let imageUrls=this.state.ImageUrl;
+        if (url){
+            imageUrls.push(url)
+        }
+        this.setState({
+            ImageUrl:imageUrls
+        });
+        return {
+            url:url
+        }
+
+    }
+
+    updateType(type){
+        this.setState({
+            type:type
+        })
+    }
+
+    saveMind(){
+        // todo 这里编写保存的效果
     }
 
     render() {
+        const TypeMap=[
+            {
+                component:<SmileOutline className={"Icon"} />,
+                type:"Happy"
+            },
+            {
+                component: <FrownOutline className={"Icon"} />,
+                type:"Unhappy"
+            },
+            {
+                component: <StarOutline className={"Icon"} />,
+                type:"Star"
+            },
+            {
+                component: <HeartOutline className={"Icon"} />,
+                type:"Important"
+            }
+        ];
+
         return (
             <div className={"container Mind"}>
                 <AutoCenter>
-                <Grid columns={2} gap={30}>
-                    <Grid.Item>
-                        <SmileOutline
-                            className={"Icon"}
-                            style={{backgroundColor:"#87d068"}}
-                        />
-                    </Grid.Item>
-                    <Grid.Item>
-                        <FrownOutline
-                            style={{backgroundColor:"#108ee9"}}
-                            className={"Icon"}
-                        />
-                    </Grid.Item>
-                    <Grid.Item>
-                        <StarOutline
-                            style={{backgroundColor:"#ff8f1f"}}
-                            className={"Icon"}
-                        />
-                    </Grid.Item>
-                    <Grid.Item>
-                        <HeartOutline
-                            style={{backgroundColor:"#ff3141"}}
-                            className={"Icon"}
-                        />
-                    </Grid.Item>
+                <Grid columns={4} gap={10}>
+                    {
+                        TypeMap.map((itemType)=>{
+                            return(
+                                <Grid.Item>
+                                    <div
+                                        onClick={()=>{
+                                            this.updateType(itemType.type)
+                                        }}
+                                        className={itemType.type+" Icon"}
+                                        style={{color:this.state.type==itemType.type?'white':'black'}}
+                                    >
+                                        {itemType.component }
+                                    </div>
+                                </Grid.Item>
+                            )
+                        })
+                    }
                 </Grid>
+                </AutoCenter>
+                <Divider />
+                <AutoCenter>
+                    <div
+                        className={this.state.type}
+                    >
+                        <Rate
+                            style={{
+                                '--inactive-color':"white"
+                            }}
+                            count={5}
+                            value={this.state.rate}
+                            onChange={(newValue)=>{
+                                this.setState({
+                                    rate:newValue
+                                })
+                            }}
+                        />
+                    </div>
                 </AutoCenter>
                 <Divider />
                 <TextArea
                     placeholder={"Input your feeling"}
-                    rows={8}
+                    rows={4}
+                    className={"InputNote"}
+                    value={this.state.note}
+                    onChange={(newValue)=>{
+                        this.setState({
+                            note:newValue
+                        });
+                    }}
                 />
                 <Divider />
                 <AutoCenter>
                     <Button
-                        size={"large"}
+                        size={"mini"}
                         color={"primary"}
+                        onClick={()=>{
+                            this.saveMind();
+                        }}
                     >
                         Save
                     </Button>
+                </AutoCenter>
+                <Divider />
+                <AutoCenter>
+                    <ImageUploader
+                        upload={(file)=>{
+                            return this.HandleUpload(file);
+                        }}>
+                        <CameraOutline className={"Icon"} />
+                    </ImageUploader>
                 </AutoCenter>
             </div>
         );
