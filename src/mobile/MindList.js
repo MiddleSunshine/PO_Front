7183d18ad9315f098afca4d2d66918d52cb05248
yangqingxian <qingxianyang@gmail.com>
2022-11-10 19:41:41 -0800

    从后台获取数据

--- src/mobile/MindList.js
+++ src/mobile/MindList.js
@@ -1,11 +1,34 @@
 import {useEffect, useState} from 'react'
 import "../css/Mind.css";
-import {List, Rate} from "antd-mobile";
+import {List, Rate, Toast} from "antd-mobile";
 import {PictureOutline} from 'antd-mobile-icons'
 import {TypeIconMap} from "./Mind";
+import {requestApi} from "../config/functions";
 
 const MindList = (props) => {
     const [mindList, updateMindList] = useState([]);
+    const [startTime,updateStartTime]=useState('');
+    const [endTime,updateEndTime]=useState('');
+
+    useEffect(()=>{
+        getMindList();
+    },[mindList])
+
+    const getMindList=(StartTime='',EndTime='')=>{
+        requestApi("/index.php?action=Feeling&method=List&StartTime"+StartTime+"&EndTime="+EndTime)
+            .then((res)=>{
+                res.json().then((json)=>{
+                    if (json.Status==1){
+                        updateMindList(json.Data);
+                    }else{
+                        Toast.show({
+                            content:json.Message,
+                            icon:"fail"
+                        })
+                    }
+                })
+            })
+    }
 
     return <div className={"container Mind"}>
         <div
