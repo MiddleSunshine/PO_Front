5ca44710437b436883e6a1c8b137920e59ae7a89
qingxianyang <qingxianyang@gmail.com>
2022-12-03 20:39:18 +0800

    部分内容完成

--- src/mobile/ScareList.js
+++ src/mobile/ScareList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import TopBar from "./component/TopBar";
-import {Divider} from "antd-mobile";
+import {Collapse, Divider, Ellipsis} from "antd-mobile";
 import {requestApi} from "../config/functions";
 
 class ScareList extends React.Component{
@@ -40,6 +40,46 @@ class ScareList extends React.Component{
                     New Reason
                 </a>
             </Divider>
+            <Collapse>
+                {
+                    this.state.ScareList.map((scare)=>{
+                        return (
+                            <Collapse.Panel
+                                key={scare.ID}
+                                title={
+                                    <div>
+                                        <Ellipsis
+                                            content={scare.Scare}
+                                            rows={1}
+                                        />
+                                    </div>
+                                }
+                            >
+                                <a href={"/Mobile/ScareList/"+scare.ID}>
+                                    Sub Scare
+                                </a>
+                                <Divider />
+                                {
+                                    scare.Explains.map((explain)=>{
+                                        return (
+                                            <div>
+                                                <Ellipsis
+                                                    key={explain.ID}
+                                                    content={explain.Explain}
+                                                    rows={1}
+                                                    expandText={'more'}
+                                                    collapseText={'less'}
+                                                />
+                                                <Divider />
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </Collapse.Panel>
+                        )
+                    })
+                }
+            </Collapse>
 
         </div>
     }
