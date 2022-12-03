c31a3dc3c8c3625742d380331ce11d63f77b2052
qingxianyang <qingxianyang@gmail.com>
2022-12-03 20:04:34 +0800

    部分内容

--- src/mobile/NewScare.js
+++ src/mobile/NewScare.js
@@ -1,8 +1,66 @@
 import React from "react";
+import TopBar from "./component/TopBar";
+import {Divider, TextArea} from "antd-mobile";
+import {message} from "antd";
+import {requestApi} from "../config/functions";
+
 
 class NewScare extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            Scare:"",
+            PScareID:props.match.params.ScareID
+        }
+        this.NewScare=this.NewScare.bind(this);
+    }
+
+    NewScare(Scare,PID){
+        if (!Scare){
+            message.warn("Please input the scare");
+            return false;
+        }
+        requestApi("/index.php?action=Scare&method=NewScare",{
+            method:"post",
+            body:JSON.stringify({
+                Scare:Scare,
+                PID:PID
+            })
+        }).then((response)=>{
+            response.json().then((json)=>{
+                if (json.Status==1){
+                    message.success("Recorded")
+                }else{
+                    message.warn(json.Message);
+                }
+            })
+        })
+    }
+
     render() {
-        return null
+        return <div className={"container"}>
+            <TopBar
+                title={"New Scare"}
+            />
+            <Divider>
+                <span
+                    onClick={()=>{
+                        this.NewScare(this.state.Scare,this.state.PScareID)
+                    }}
+                >Save</span>
+            </Divider>
+            <TextArea
+                placeholder={"Input your scare"}
+                rows={10}
+                value={this.state.Scare}
+                onChange={(newValue)=>{
+                    this.setState({
+                        Scare:newValue
+                    })
+                }}
+            />
+
+        </div>
     }
 }
 
--- src/mobile/ScareList.js
+++ src/mobile/ScareList.js
@@ -3,13 +3,19 @@ import TopBar from "./component/TopBar";
 import {Divider} from "antd-mobile";
 
 class ScareList extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            ScareList:[]
+        }
+    }
     render() {
         return <div>
             <TopBar
                 title={"Life"}
             />
             <Divider>
-                <a href={""}>
+                <a href={"/Mobile/NewScare/0"}>
                     New Reason
                 </a>
             </Divider>
