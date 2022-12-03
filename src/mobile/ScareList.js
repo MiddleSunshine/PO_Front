c0170bb0a5ce9d31624c806a987b752c30059da1
qingxianyang <qingxianyang@gmail.com>
2022-12-03 20:19:04 +0800

    部分内容完成

--- src/mobile/ScareList.js
+++ src/mobile/ScareList.js
@@ -1,21 +1,42 @@
 import React from 'react'
 import TopBar from "./component/TopBar";
 import {Divider} from "antd-mobile";
+import {requestApi} from "../config/functions";
 
 class ScareList extends React.Component{
     constructor(props) {
         super(props);
         this.state={
-            ScareList:[]
+            ScareList:[],
+            ScarePID:props.match.params.ScareID
         }
+        this.getScareList=this.getScareList.bind(this);
     }
+
+    componentDidMount() {
+        this.getScareList(this.state.ScarePID);
+    }
+
+    getScareList(PID){
+        requestApi("/index.php?action=Scare&method=ScareList&PID="+PID)
+            .then((res)=>{
+                res.json().then((json)=>{
+                    if (json.Status==1){
+                        this.setState({
+                            ScareList:json.Data.ScareList
+                        })
+                    }
+                })
+            })
+    }
+
     render() {
         return <div>
             <TopBar
                 title={"Life"}
             />
             <Divider>
-                <a href={"/Mobile/NewScare/0"}>
+                <a href={"/Mobile/NewScare/"+this.state.ScarePID}>
                     New Reason
                 </a>
             </Divider>
